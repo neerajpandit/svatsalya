07e26b229c4db8ef2902f427ea1491f154d7d7d1
Neeraj Pandey <106172268+neerajpandit@users.noreply.github.com>
2024-04-24 17:44:46 +0530

    Update settings.py

--- vatsalya/settings.py
+++ vatsalya/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-f-lg!*t&^60&1h5cnslqqw0^qra)4@rjkmvxd-iv4e%913d7&('
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS","*").split(" ")
